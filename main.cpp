fad5209d26f0a168fe553c8e16ad60aafd814998
Alioun <alioun.diagne@gmail.com>
2018-06-15 10:52:02 +0200

    fixed typos

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
@@ -11,7 +12,7 @@
  * @param size  size of graph and color-array
  * @param graph graph.
  */
-void printit(int *color, int size, bool **graph){
+void printit(int *color, int size, bool **graph, bool toPicture){
     // init a few colors.
     std::string* pStr = new std::string[8] { "red", "blue", "yellow", "green", "grey", "chocolate", "cyan", "orange" };
     // init the writer
@@ -32,9 +33,10 @@ void printit(int *color, int size, bool **graph){
     out << "}" << std::endl;
     out.close();
 
-    std::system("dot -Tpng output.dot -o out.png");
-    std::system("feh out.png");
-
+    if(toPicture) {
+        std::system("dot -Tpng output.dot -o out.png");
+        std::system("feh out.png");
+    }
 
 
 }
@@ -94,33 +96,37 @@ int main()
 {
 
 
-    std::cout<<"enter number of edges"<<std::endl;
-    int numEdge;
-    std::cin>>numEdge;
+    std::cout<<"enter number of nodes"<<std::endl;
+    int numNode;
+    std::cin>>numNode;
 
     // init values
-    bool te2[numEdge][numEdge];
+    bool te2[numNode][numNode];
     bool **te;
     int val;
 
 
 
     // get all the graph-input
-    std::cout<<"Your graph has " << numEdge << " Edges." << std::endl;
-    std::cout<<"Please enter the Connections each edge has, set 1 for a connection, 0 for none." << std::endl;
+    std::cout<<"Your graph has " << numNode << " Nodes." << std::endl;
+    std::cout<<"Please enter the Connections each node has, set 1 for a connection, 0 for none." << std::endl;
     std::cout<<"Please seperate your input with a blank." << std::endl;
-    std::cout<<"0 1 2 3 4" << std::endl;
-    te = (bool **) malloc(numEdge * sizeof(bool*));
+    std::string nodes = "   ";
+    for (int i = 0; i<numNode; i++){
+        nodes += std::to_string(i) + " ";
+    }
+    std::cout<< nodes << std::endl;
+    te = (bool **) malloc(numNode * sizeof(bool*));
 
     // instanciate the array
-    for (int row = 0; row<numEdge; row++) {
-        te[row] = (bool *) malloc(numEdge * sizeof(bool));
+    for (int row = 0; row<numNode; row++) {
+        te[row] = (bool *) malloc(numNode * sizeof(bool));
     }
 
-    int temEdge = numEdge;
-    for(int j=0;j<numEdge;j++){
-        bool temparray[numEdge];
-        for(int i=0; i<numEdge; i++) {
+    int temEdge = numNode;
+    for(int j=0;j<numNode;j++){
+        bool temparray[numNode];
+        for(int i=0; i<numNode; i++) {
             std::cin >> val;
             if (val != 0 and val != 1) {
                 std::cout << "You entered a invalid number, please start over." << std::endl;
@@ -138,15 +144,15 @@ int main()
 
     // call the function that does the coloring.
     // initialize the color-array, one color for each
-    int colors[numEdge];
+    int colors[numNode];
     // all color-vaules are initialized to 0.
     for (int i = 0; i < numColors; i++)
         colors[i] = 0;
     // call with edge 0 as starting point.
-    bool tmp = recColors(te, numColors, colors, 0, numEdge);
-    //color(numColors, te, numEdge);
+    bool tmp = recColors(te, numColors, colors, 0, numNode);
+    //color(numColors, te, numNode);
     if (tmp == 1)
-        printit(colors, numEdge, te);
+        printit(colors, numNode, te, false);
     else {
         std::cout << "graph g{" << std::endl;
         std::cout << "Failed [style=filled, fillcolor=\"red\"]" << std::endl;
